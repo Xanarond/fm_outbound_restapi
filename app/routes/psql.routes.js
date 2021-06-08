7280d038df4c09f387352037bd7600cc7a094662
Vlad Kostyukhin <kostyukhin.v@EU.corp.samsungelectronics.net>
2021-06-08 09:58:37 +0300

    Add second promise

--- app/routes/psql.routes.js
+++ app/routes/psql.routes.js
@@ -7,11 +7,11 @@ const env = require("../config/psql_config");
  */
 exports.getPackingShifts = (req, res) => {
   const conn = `postgres://${env.username}:${env.password}@${env.host}/${env.database}`;
-  let result = {};
-  const total = [];
+  const all_result = {};
+  const currentDateresult = [];
   const p1 = new Promise((resolve, reject) => {
     try {
-      const st_arr = req.query.period.split("-");
+      const st_arr = req.query.cur_date.split("-");
       const result_per = `${st_arr[2]}.${st_arr[1]}.${st_arr[0]}`; // formatting date to required format
 
       console.log(result_per);
@@ -19,21 +19,44 @@ exports.getPackingShifts = (req, res) => {
       const client = new Client({ connectionString: conn }); // instance of connection PostgreSQL
       client.connect();
 
-      const sql_req = `SELECT Distinct * from "public".packing_shifts WHERE pack_date ='${result_per}'`;
-      // eslint-disable-next-line max-len
-      client.query(sql_req, (err, result) => (err ? console.log(err.stack) : total.push(result.rows)));
+      const sql_req = `SELECT Distinct pack_time, result from "public".packing_shifts WHERE pack_date ='${result_per}' and pack_time <='19:00:00' `;
+      client.query(sql_req, (err, result) => (err ? console.log(err.stack) : currentDateresult.push(result.rows)));
 
-      setTimeout(() => resolve(total), 1000); // add timeout for load data
+      setTimeout(() => resolve(currentDateresult), 1000); // add timeout for load data
       setTimeout(() => reject(new Error("Something went wrong!")), 1000);
     } catch (e) {
       console.log(e);
     }
   });
-  p1.then((values) => {
-    result = values;
-    console.log(result);
-    res.send(result);
-  }).catch((e) => {
-    console.log(e, "No response from the server");
+
+  const pastDateresult = [];
+  const p2 = new Promise((resolve, reject) => {
+    try {
+      const st_arr = req.query.past_date.split("-");
+      const result_per = `${st_arr[2]}.${st_arr[1]}.${st_arr[0]}`; // formatting date to required format
+
+      console.log(result_per);
+
+      const client = new Client({ connectionString: conn }); // instance of connection PostgreSQL
+      client.connect();
+
+      const sql_req = `SELECT Distinct pack_time, result from "public".packing_shifts WHERE pack_date ='${result_per}' and pack_time between '20:00:00' and '23:00:00'`;
+      client.query(sql_req, (err, result) => (err ? console.log(err.stack) : pastDateresult.push(result.rows)));
+
+      setTimeout(() => resolve(pastDateresult), 1000); // add timeout for load data
+      setTimeout(() => reject(new Error("Something went wrong!")), 1000);
+    } catch (e) {
+      console.log(e);
+    }
   });
+  Promise.all([p1, p2])
+    .then(([firstp, secondp]) => {
+      all_result.cur_date = firstp;
+      all_result.past_date = secondp;
+      console.log(all_result.cur_date, all_result.past_date);
+      res.send(all_result);
+    })
+    .catch((e) => {
+      console.log(e, "No response from the server");
+    });
 };
